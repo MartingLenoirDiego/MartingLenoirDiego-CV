d44d90a53d936c5b33f1c516a2e604e69c3054a8
Marting Lenoir Diego <44231661+MartingLenoirDiego@users.noreply.github.com>
2022-05-02 14:55:36 +0200

    complet

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,14 @@ AOS.init({
   anchorPlacement: 'top-left',
   duration: 1000
 });
+
+
+
+function modal(id){
+  var modal = document.getElementById("modal"+id);
+  modal.style.display = "block";
+}
+function span(id){
+  var modal = document.getElementById("modal"+id);
+  modal.style.display = "none";
+}
